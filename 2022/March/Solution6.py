16634daba4532a6f0b67121ac5f2de30beca54a4
Mik3Qiao <mike0524745@gmail.com>
2022-03-07 19:24:35 -0600

    740. Delete and Earn 141. Linked List Cycle

--- 2022/March/Solution6.py
+++ 2022/March/Solution6.py
@@ -7,10 +7,21 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        checked = set()
-        while head is not None:
-            if head in checked:
-                return True
-            checked.add(head)
-            head = head.next
-        return False
+        # checked = set()
+        # while head is not None:
+        #     if head in checked:
+        #         return True
+        #     checked.add(head)
+        #     head = head.next
+        # return False
+        if head is None:
+            return False
+        left = head
+        right = head.next
+        while left != right:
+            if right is None or right.next is None:
+                return False
+            left = left.next
+            right = right.next.next
+
+        return True
